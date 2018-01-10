779a838d2b15dc41e31e765bdc7f2afcdd352307
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-01-10 01:33:12 +0100

    Lower required elixir version to 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Namae.Mixfile do
     [
       app: :namae,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       preferred_cli_env: [espec: :test]
